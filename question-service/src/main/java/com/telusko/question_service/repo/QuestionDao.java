4ac99fc7754c30440e715318b28c3f27c20ab4c5
deepak2429 <deep242910@gmail.com>
2025-09-13 19:27:49 +0530

    1. question service

---
+++ question-service/src/main/java/com/telusko/question_service/repo/QuestionDao.java
@@ -0,0 +1,22 @@
+package com.telusko.question_service.repo;
+
+import com.telusko.question_service.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
+
+//    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+//    List<Question> findRandomQuestionsByCategory(String category, int numQ);
+
+    @Query(value = "SELECT * FROM question q WHERE q.category IN :categories ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionsByCategories(List<String> categories, int numQ);
+
+
+}
